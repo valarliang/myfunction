1a9dc5b93e06698ae3bbced8482b3052292df2e9
梁琰沁 <1780304097@qq.com>
2019-03-05 18:02:10 +0800

    adsfs

--- myfunction.js
+++ myfunction.js
@@ -307,13 +307,18 @@ const throttle = (func, wait = 50) => {
     }
   }
 }
-
-function bubleSort(arr) {
+// 优点：稳定、可对链表
+function bubbleSort(arr) {
   let len = arr.length;
-  for (let i = 0; i < len; i++) {
-    for (let j = 0; j < len - i - 1; j++) {
-      if (arr[j] > arr[j+1]) [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+  for (let i = len - 1; i >= 0; i--) {
+    let flag = 0;
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j+1]) {
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+        flag = 1;
+      }
     }
+    if (!flag) break;
   }
   return arr;
 }
